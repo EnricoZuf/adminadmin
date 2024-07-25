cb0fcdbca4cc8537d4af735737b8fc6d2dd05e86
Enrico <enrico.zuffellato@tutanota.com>
2024-07-25 00:32:55 -0300

    [ADD] - add functions readUser, readAllUsers, searchUsers and their routes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 const { User } = require('../models');
+const { Op } = require('sequelize');
 
 class UserController {
     //Create user
@@ -9,23 +10,128 @@ class UserController {
             if(!firstName || !lastName || !email || !password || !birthDate){
                 return res.status(400).json({error: 'All fields are required.'});
             }
-                
-            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-            const birthdateRegex = /^\d{4}-\d{2}-\d{2}$/;
+            
+            const lowerCaseEmail = email.toLowerCase();
+            const existingUser = await User.findOne({where: {email: lowerCaseEmail}});
+            if (existingUser){
+                return res.status(400).json({error: `'${lowerCaseEmail}' already exists.`});
+            }
 
-            if(!emailRegex.test(email)){
-                return res.status(400).json({error: `'${email}' invalid e-mail format.`});
-            }else if(!birthdateRegex.test(birthDate)){
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if(!emailRegex.test(lowerCaseEmail)){
+                return res.status(400).json({error: `'${lowerCaseEmail}' invalid e-mail format.`});
+            }
+            
+            const birthdateRegex = /^\d{4}-\d{2}-\d{2}$/;
+            if(!birthdateRegex.test(birthDate)){
                 return res.status(400).json({error: `invalid bithdate format. Use YYYY-MM-DD`});
             }
 
-            const user = await User.create({ firstName, lastName, email, password, birthDate});
+            const capitalizedFirstName = firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
+            const capitalizedLastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase();
+
+            const user = await User.create({ firstName: capitalizedFirstName, lastName:capitalizedLastName, email: lowerCaseEmail, password, birthDate});
             res.status(201).json(user);
 
         }catch(e){
             res.status(500).json({ error: e.message});
         }
     }
+
+    //Read user
+    static async readUser(req,res){
+        try{
+            const { email } = req.params;
+
+            if(!email){
+                return res.status(400).json({error: 'invalid email'});
+            }
+
+            const lowerCaseEmail = email.toLowerCase();
+            const existingUser = await User.findOne({
+                where: {
+                    email: lowerCaseEmail
+                },
+                attributes: {
+                    exclude: ['password'] 
+                }
+            });
+            if(!existingUser){
+                return res.status(400).json({error: 'unregistered email'});
+            }
+
+           return res.status(200).json(existingUser);
+
+        } catch (e){
+            res.status(500).json({error: e.message});
+        }
+    }
+
+    //read all users
+    static async readAllUsers(req,res){
+        try{
+            
+            const users = await User.findAll({
+                attributes: {
+                    exclude: ['password'] 
+                }
+            });
+            return res.status(200).json(users);
+
+        } catch (e){
+            res.status(500).json({error: e.message});
+        }
+    }
+
+        //Search users
+        static async searchUsers(req,res){
+            try{
+            
+                const { query } = req.query;
+                
+                if (!query) {
+                    return res.status(400).json({ error: 'Query parameter is required.' });
+                }
+
+                const lowerCaseQuery = query.toLowerCase();
+                const users = await User.findAll({
+                    where: {
+                        [Op.or]: [
+                            {
+                                email: {
+                                    [Op.like]: `%${lowerCaseQuery}%`
+                                }
+                            },
+                            {
+                                firstName: {
+                                    [Op.like]: `%${lowerCaseQuery}%`
+                                }
+                            },
+                            {
+                                lastName: {
+                                    [Op.like]: `%${lowerCaseQuery}%`
+                                }
+                            }
+                        ]
+                    },
+                    attributes: {
+                        exclude: ['password'] 
+                    }
+                })
+
+                if (users.length > 0) {
+                    return res.status(200).json(users);
+                } else {
+                    return res.status(404).json({ error: 'No users found.' });
+                }
+
+
+            } catch (e){
+                res.status(500).json({error: e.message});
+            }
+        }
+    
+    
 }
 
 module.exports = UserController;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const userController = require('../controllers/userController');
 const router = express.Router();
 
+router.get('/user/:email', userController.readUser);
+router.get('/users/:param', userController.searchUsers);
+router.get('/user', userController.readAllUsers);
+
 router.post('/user', userController.createUser);
 
 module.exports = router;

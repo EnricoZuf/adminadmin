b05628c1352bf02db16e7a9d262eda1af6254f65
Enrico <enrico.zuffellato@tutanota.com>
2024-08-24 23:59:46 -0300

    [MOD] - add encoder to communication between views and controllers

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './src/views');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
